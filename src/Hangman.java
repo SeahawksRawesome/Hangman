40fd9d0730bf31210b60bc5001d753e26d15766f
SeahawksRawesome <btpowell66@cox.net>
2018-04-01 17:25:59 -0700

    On Hangman, did step 1-2, on three. Making the GUI now
    COOL

--- src/Hangman.java
+++ src/Hangman.java
@@ -3,25 +3,38 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Hangman {
 
 	public static void main(String[] args) {
+		Stack<String> wordStack = new Stack<String>();
 		ArrayList<String> words = new ArrayList<String>();
-
+		Random wordChooser = new Random();
 		String numsParse = JOptionPane.showInputDialog("Pick A number, any number fo.");
-
-		for (int i = 0; i < numsParse.length(); i++) {
-
-			String fileContent = "";
+		int wordAmount = Integer.parseInt(numsParse);
+		String fileContent = "";
+		String line = "";
+		int Lives = 10;
+		String word = "";
+		
+		for (int i = 0; i < wordAmount; i++) {
 			try {
 				BufferedReader dictionaryReader = new BufferedReader(new FileReader("src//dictionary.txt"));
+				int randWord = wordChooser.nextInt(2999);
 
-				String line = dictionaryReader.readLine();
-				words.add(line);
-
+				for (int x = 0; x < randWord; x++) {
+					dictionaryReader.readLine();
+				}
+				line = dictionaryReader.readLine();
+				// words.add(line);
+				wordStack.push(line);
 				dictionaryReader.close();
 			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
@@ -32,8 +45,20 @@ public class Hangman {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		System.out.println(words);
 
+		}
+		Hangman GUI = new Hangman();
+	}
+	void GUI(){
+		JFrame f1 = new JFrame();
+		JPanel p1 = new JPanel();
+		JLabel GuessWord = new JLabel();
+		JLabel word = new JLabel();
+		JLabel Lives = new JLabel();
+		
+		f1.add(p1);
+		p1.add(Lives);
+		p1.add(word);
+		p1.add(GuessWord);
 	}
 }
